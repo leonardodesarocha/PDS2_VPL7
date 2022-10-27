8556a0a6f688e52297e741725550cb541ea51e55
LeonardoDTS <112580596+LeonardoDTS@users.noreply.github.com>
2022-10-27 19:15:14 -0300

    Update intruso.cpp

--- intruso.cpp
+++ intruso.cpp
@@ -1,7 +1,20 @@
 #include "intruso.hpp"
 
-void Intruso::set_senha_vazada(std::string vazou) {
+void Intruso::set_senha_vazada(std::string vazou)
+{
+    std::string auxNumeros;
+    std::string auxLetras;
 
+    for (int i = 0; i <= 18; i = i + 2)
+    {
+        auxNumeros += vazou[i];
+    }
+    for (int i = 20; i <= 30; i = i + 2)
+    {
+        auxLetras += vazou[i];
+    }
+    _seqNumDiario.push_back(auxNumeros);
+    _seqLetrasDigitadas.push_back(auxLetras);
 }
 
 std::string Intruso::crack_senha() {
